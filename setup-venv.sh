6cfe30a56959107479d4288cac86a6659f667fc1
2549239 <146085622+2549239@users.noreply.github.com>
2024-01-24 18:55:43 +0000

    Update setup-venv.sh

--- setup-venv.sh
+++ setup-venv.sh
@@ -2,6 +2,7 @@
 cd /var/lib/jenkins/workspace/myflix-jenkins
 python3 -m venv myenv
 source myenv/bin/activate
+pip install -r requirements.txt
 python3 manage.py
 python3 manage.py collectstatic --noinput
 
