d4385104691242e3867a0251e4bd0d1f755286f8
Ogechukwu Ike <2549239@dundee.ac.uk>
2024-01-24 19:17:05 +0000

    update

--- setup-venv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /var/lib/jenkins/workspace/myflix-jenkins
-python3 -m venv myenv
-source myenv/bin/activate
-pip install -r requirements.txt
-python3 manage.py
-python3 manage.py collectstatic --noinput
-
